91a6cb781f558c6d268fbc17f4253f522f031352
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-12-24 15:11:39 +1100

    feat: build footer
    footer has navigation links to other pages, as well as contact information. i did not include a cta in the footer because the header is sticky (if that changes, then the CTA button should also be in footer). fixes #5. privacy policy not included yet because this needs discussion.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => (
   <>
     <Header />
-    
+
     <main>
       <Outlet context={[]} />
     </main>
+
+    <Footer />
   </>
 );
 
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,129 @@
+import { Link } from "react-router-dom";
+import {
+  IconCopyright,
+  IconMapPin,
+  IconMail,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
+import brandingImage from "/images/branding/logo-black-bg.png";
+import styles from "./Footer.module.css";
+
+const FOOTER_LINKS = [
+  {
+    heading: "Browse",
+    links: [
+      { path: "/", label: "Home" },
+      { path: "/timetable", label: "Timetable" },
+      { path: "/contact", label: "Contact" },
+    ],
+  },
+  {
+    heading: "Classes",
+    links: [
+      { path: "/kids-classes", label: "Kids" },
+      { path: "/adults-classes", label: "Adults" },
+      { path: "/girls-and-women-classes", label: "Girls and women" },
+    ],
+  },
+  {
+    heading: "Contact",
+    items: [
+      {
+        label: "Location",
+        icon: <IconMapPin size="100%" />,
+        value: `18/390 Eastern Valley Way
+Chatswood NSW 2067`,
+      },
+      {
+        label: "Email",
+        icon: <IconMail size="100%" />,
+        value: "fang@equinoxacademy.com.au",
+      },
+    ],
+  },
+  {
+    heading: "Follow us",
+    links: [
+      {
+        label: "Instagram",
+        icon: <IconBrandInstagram size="100%" />,
+        link: "https://www.instagram.com/equinoxmartialarts/",
+      },
+    ],
+  },
+];
+
+const Footer = () => {
+  const footerColumns = FOOTER_LINKS.map((column) => {
+    let columnContent = [];
+
+    if (column.heading === "Contact")
+      columnContent = column.items.map((item) => (
+        <div
+          className={styles.contactItem}
+          aria-label={item.label}
+          key={item.label}
+        >
+          <figure className={styles.icon}>{item.icon}</figure>
+          <p className={styles.locationValue}>{item.value}</p>
+        </div>
+      ));
+    else if (column.heading === "Follow us")
+      columnContent = column.links.map((item) => (
+        <a
+          href={item.link}
+          aria-label={`Visit our ${item.label}`}
+          target="_blank"
+          className={styles.socialsIcon}
+        >
+          {item.icon}
+        </a>
+      ));
+    else
+      columnContent = column.links.map((link) => (
+        <Link
+          to={link.path}
+          aria-label={`Go to ${link.label} page`}
+          key={link.label}
+        >
+          {link.label}
+        </Link>
+      ));
+
+    return (
+      <div className={styles.column}>
+        <h2 className={styles.heading}>{column.heading}</h2>
+        {columnContent}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={styles.footer}>
+      <section className={styles.top}>
+        <Link
+          to="/"
+          aria-label="Go to home page"
+          className={styles.brandingLink}
+        >
+          <img
+            src={brandingImage}
+            alt="Logo for Equinox Academy"
+            className={styles.brandingImage}
+          />
+        </Link>
+
+        {footerColumns}
+      </section>
+
+      <section className={styles.bottom}>
+        <div className={styles.inner}>
+          <IconCopyright size={16} className={styles.copyrightIcon} />
+          <small>2025 Equinox Academy. All rights reserved.</small>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
