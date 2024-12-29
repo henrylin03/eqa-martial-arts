3ba8206dacb6204521cebd7c88cd2ceb069b6e48
Ketan Sahu <120404520+ketansahugit@users.noreply.github.com>
2024-12-29 10:31:45 +0000

    Feature- Add Scroll to top functionality on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const App = () => (
   <>
     <Header />
 
     <main>
+      <ScrollToTop />
       <Outlet />
     </main>
 
