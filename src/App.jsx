ea6d37fb11b2f47781e2ac997117c6349149ca56
Ketan Sahu <120404520+ketansahugit@users.noreply.github.com>
2025-01-01 18:16:24 +0000

    replace scrollToTop component with ScrollRestoration for cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const App = () => (
   <>
     <Header />
 
     <main>
-      <ScrollToTop />
+      <ScrollRestoration />
       <Outlet />
     </main>
 
