21254aaadc53228929748d9bc6a48c259d53b37a
Ketan Sahu <120404520+ketansahugit@users.noreply.github.com>
2025-01-02 08:59:41 +0530

    feat: when user clicks on any Router Links or NavLinks, the user is taken to the top of the page
    this is especially useful when user is navigating to another page via the footer - we'd want the user to be taken to the top of the page, rather than the footer of that page, or somewhere in the middle of the target page. this improves UX. fixes #26.
    
    * Feature- Add Scroll to top functionality on route changes
    
    * replace scrollToTop component with ScrollRestoration for cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -7,6 +7,7 @@ const App = () => (
     <Header />
 
     <main>
+      <ScrollRestoration />
       <Outlet />
     </main>
 
