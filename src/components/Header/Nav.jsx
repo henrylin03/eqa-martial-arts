henrylin03/eqa-martial-arts

commit 4439a2006b74b8274425460c22071042b001775a

style: when the user is currently on one of the classes pages, then the 'classes' link in the navbar is styled accordingly. on narrower screens, the chevron for classes subpages is already opened.
this fixes #22. this improves UX because user can know where they are on the site when on a classes page, rather than having to hover over the classes navlink on wider screens, or clicking open the chevron on the drawer on narrower screens, to know where they are.

src/components/Header/Nav.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useDisclosure } from "@mantine/hooks";
 import { UnstyledButton, Collapse, Menu } from "@mantine/core";
 import { IconChevronDown } from "@tabler/icons-react";
@@ -21,7 +21,16 @@ const PAGE_LINKS = [
 ];
 
 const Nav = ({ isNarrowScreen = false, closeDrawer = () => {} }) => {
+  const currentLocation = useLocation();
   const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+
+  const isClassPage = PAGE_LINKS.find(
+    (link) =>
+      link.label === "Classes" &&
+      link.sublinks.some(
+        (sublink) => currentLocation.pathname === sublink.path,
+      ),
+  );
 
   const navLinks = PAGE_LINKS.map((linkObject) => {
     if (!linkObject.hasOwnProperty("sublinks"))
@@ -42,7 +51,9 @@ const Nav = ({ isNarrowScreen = false, closeDrawer = () => {} }) => {
       return (
         <div key={linkObject.label} className={styles.sublinkNarrowWrapper}>
           <UnstyledButton onClick={toggleLinks} className={styles.sublinkLabel}>
-            <span>{linkObject.label}</span>
+            <span className={isClassPage ? styles.activeLink : undefined}>
+              {linkObject.label}
+            </span>
             <IconChevronDown
               size={16}
               className={
@@ -53,7 +64,7 @@ const Nav = ({ isNarrowScreen = false, closeDrawer = () => {} }) => {
             />
           </UnstyledButton>
 
-          <Collapse in={linksOpened}>
+          <Collapse in={isClassPage ? true : linksOpened}>
             <div className={styles.sublinksDrawer}>
               {linkObject.sublinks.map((sublink) => (
                 <NavLink
@@ -81,7 +92,9 @@ const Nav = ({ isNarrowScreen = false, closeDrawer = () => {} }) => {
       >
         <Menu.Target>
           <div className={styles.sublinkLabel}>
-            <span>{linkObject.label}</span>
+            <span className={isClassPage ? styles.activeLink : undefined}>
+              {linkObject.label}
+            </span>
             <IconChevronDown size={16} />
           </div>
         </Menu.Target>

commit e0845ff5ed6a3f239326e3bc54bf472dfc5872a8

fix(Nav): on narrowrower screens, classes' sublinks are no longer opened when on a classes page. previously, there was a bug introduced in 4439a2006 where the classes sublinks were opened but it couldn't be closed.

src/components/Header/Nav.jsx
@@ -64,7 +64,7 @@ const Nav = ({ isNarrowScreen = false, closeDrawer = () => {} }) => {
             />
           </UnstyledButton>
 
-          <Collapse in={isClassPage ? true : linksOpened}>
+          <Collapse in={linksOpened}>
             <div className={styles.sublinksDrawer}>
               {linkObject.sublinks.map((sublink) => (
                 <NavLink