henrylin03/eqa-martial-arts

commit c9c8d29daa30b478e2ce7609e0ef2ee114a3dfb9

refactor: CTA button is a separate component that passes in props (fixes #33). feat: CTA button now links to Clubworx for user to sign waiver. refactor: above the fold section on most sub pages (except contact at the moment) is its own component (fixes #45)
this helps keep the code DRY, especially for certain props for Mantine components that repeated, like the colour and radius, as well as the CSS classes and JSX markup for the above the fold sections across the pages. This improves maintainability greatly, as well as making the pages themselves more readable.

src/components/CallToActionButton/index.jsx
@@ -1,21 +1,43 @@
 import { Button } from "@mantine/core";
 import PropTypes from "prop-types";
 
-const CallToActionButton = ({ className, isNarrowScreen = false }) => (
-  <Button
-    variant="filled"
-    color="orange"
-    radius="xl"
-    className={className}
-    size={isNarrowScreen ? "xs" : "md"}
-  >
-    {isNarrowScreen ? "Free trial" : "Book a free trial"}
-  </Button>
-);
+const CallToActionButton = ({
+  variant = "filled",
+  size = "sm",
+  customCssClass = undefined,
+  buttonText = "Book a free trial",
+}) => {
+  return (
+    <a
+      href="https://app.clubworx.com/websites/equinox-martial-arts-academy/waivers/trial-pass-waiver/signed_waivers/new"
+      target="_blank"
+      aria-label="Sign up for a free trial in a new tab"
+    >
+      <Button
+        color="orange"
+        radius="xl"
+        variant={variant}
+        size={size}
+        className={customCssClass}
+      >
+        {buttonText}
+      </Button>
+    </a>
+  );
+};
 
 CallToActionButton.propTypes = {
-  className: PropTypes.string,
-  isNarrowScreen: PropTypes.bool,
+  variant: PropTypes.oneOf([
+    "filled",
+    "light",
+    "outline",
+    "subtle",
+    "transparent",
+    "white",
+  ]),
+  size: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
+  customCssClass: PropTypes.string,
+  buttonText: PropTypes.string,
 };
 
 export default CallToActionButton;

src/components/AboveTheFoldSection/index.jsx
@@ -1,0 +1,33 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Button } from "@mantine/core";
+import CallToActionButton from "../CallToActionButton";
+import styles from "./AboveTheFoldSection.module.css";
+
+const AboveTheFoldSection = ({ pageName }) => {
+  return (
+    <section className={styles.aboveTheFold}>
+      <div className={styles.aboveTheFoldInner}>
+        <h1 className={styles.pageTitle}>{pageName}</h1>
+
+        <div className={styles.buttonGroup}>
+          <CallToActionButton />
+
+          {!pageName.toLowerCase().includes("timetable") && (
+            <Link to="/timetable" aria-label="See timetable for classes">
+              <Button variant="outline" color="orange" radius="xl" bg="white">
+                Class timetable
+              </Button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+AboveTheFoldSection.propTypes = {
+  pageName: PropTypes.string.isRequired,
+};
+
+export default AboveTheFoldSection;

src/components/CallToActionSection/index.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
-import { Button } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
+import CallToActionButton from "../CallToActionButton";
 import styles from "./CallToActionSection.module.css";
 
 const CallToActionSection = ({ isWhiteBg = false }) => {
@@ -12,14 +12,10 @@ const CallToActionSection = ({ isWhiteBg = false }) => {
     >
       <div className={styles.ctaInner}>
         <p className={styles.ctaText}>Ready to try a class?</p>
-        <Button
-          color="orange"
+        <CallToActionButton
           variant="white"
-          radius="xl"
           size={isNarrowScreen ? "sm" : "lg"}
-        >
-          Book a free trial
-        </Button>
+        />
       </div>
     </section>
   );

src/components/Header/index.jsx
@@ -70,8 +70,9 @@ const Header = () => {
         )}
 
         <CallToActionButton
-          className={styles.button}
-          isNarrowScreen={isNarrowScreen}
+          size={isNarrowScreen ? "xs" : "md"}
+          buttonText={isNarrowScreen ? "Free trial" : "Book a free trial"}
+          customCssClass={styles.button}
         />
       </div>
     </header>

src/pages/HomePage/index.jsx
@@ -84,7 +84,7 @@ const HomePage = () => {
             </p>
           </div>
 
-          <CallToActionButton />
+          <CallToActionButton size="md" />
         </div>
 
         {isNarrowScreen ? (

src/pages/TimetablePage/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Button, Skeleton } from "@mantine/core";
+import { Skeleton } from "@mantine/core";
 import CallToActionSection from "../../components/CallToActionSection";
+import AboveTheFoldSection from "../../components/AboveTheFoldSection";
 import styles from "./TimetablePage.module.css";
 
 const TimetablePage = () => {
@@ -8,23 +9,7 @@ const TimetablePage = () => {
 
   return (
     <>
-      <section className={styles.aboveTheFold}>
-        <div className={styles.aboveTheFoldInner}>
-          <h1 className={styles.pageTitle}>Timetable</h1>
-          {/* <p className={styles.aboveTheFoldText}>
-            See our regular schedule for adults and kids classes, and our{" "}
-            <a href="#live-timetable" className={styles.link}>
-              live weekly schedule
-            </a>
-            !
-          </p> */}
-          <article className={styles.ctaButtons}>
-            <Button color="orange" radius="xl">
-              Book a free trial
-            </Button>
-          </article>
-        </div>
-      </section>
+      <AboveTheFoldSection pageName="Timetable" />
 
       <section id="live-timetable" className={styles.liveTimetableSection}>
         <Skeleton visible={isLoadingLiveTimetable} radius="lg">

src/components/AboveTheFoldSection/AboveTheFoldSection.module.css
@@ -1,0 +1,22 @@
+.aboveTheFold {
+  background-color: var(--light-orange);
+}
+
+.aboveTheFoldInner {
+  padding: 60px 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+}
+
+.pageTitle {
+  font-size: 2.5rem;
+  text-align: center;
+  text-transform: capitalize;
+}
+
+.buttonGroup {
+  display: flex;
+  gap: 12px;
+}

src/pages/ClassesPage/AdultsClassesPage.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import { Button, Image } from "@mantine/core";
+import { Image } from "@mantine/core";
+import AboveTheFoldSection from "../../components/AboveTheFoldSection";
 import CallToActionSection from "../../components/CallToActionSection";
 import TickIcon from "./TickIcon";
 import styles from "./Classes.module.css";
@@ -7,27 +7,8 @@ import styles from "./Classes.module.css";
 const AdultsClassesPage = () => {
   return (
     <>
-      <section className={styles.aboveTheFold}>
-        <div className={styles.aboveTheFoldInner}>
-          <h1 className={styles.pageTitle}>Adults classes</h1>
-          <div className={styles.buttonGroup}>
-            <Button color="orange" radius="xl">
-              Book a free trial
-            </Button>
-            {/* TODO: LINK TO THE ADULTS TIMETABLE SECTION OF TIMETABLE PAGE */}
-            <Link
-              to="/timetable"
-              aria-label="See timetable for adults' classes"
-            >
-              <Button variant="outline" color="orange" radius="xl" bg="white">
-                Class timetable
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </section>
+      <AboveTheFoldSection pageName="Adults classes" />
 
-      {/* TODO: REFACTOR THE FOLLOWING INTO AN ARRAY OF OBJECTS THAT I CAN .MAP OVER */}
       <section className={styles.classTypes}>
         <article className={styles.classType}>
           <div className={styles.inner}>

commit febf40ab86de8ff11e30990ce3c0443424388652

fix(Header): alignment of CTA in header was previously not being applied. now it is justified to the right-most part of header.
this was a bug that was introduced in c9c8d29. the header is the only instance of CTA button component that has a classname attached to it to give it custom CSS styling. however, the class was applied to the child rather than the parent, so it wasn't being applied.

src/components/CallToActionButton/index.jsx
@@ -12,14 +12,9 @@ const CallToActionButton = ({
       href="https://app.clubworx.com/websites/equinox-martial-arts-academy/waivers/trial-pass-waiver/signed_waivers/new"
       target="_blank"
       aria-label="Sign up for a free trial in a new tab"
+      className={customCssClass}
     >
-      <Button
-        color="orange"
-        radius="xl"
-        variant={variant}
-        size={size}
-        className={customCssClass}
-      >
+      <Button color="orange" radius="xl" variant={variant} size={size}>
         {buttonText}
       </Button>
     </a>